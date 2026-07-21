ca35b7d954f0e647328d1aa2a1182c51ec4ebc23
Jutsu78 <loshataraev@gmail.com>
2026-07-21 18:52:04 +0300

    Add node-cron for scheduled Steam sale notifications

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 require('dotenv').config();
+const cron = require('node-cron');
 const { Client, GatewayIntentBits, Collection, Events } = require("discord.js");
+const steamSales = require('./sales.js');
 
 const bot = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
 });
 
 bot.once(Events.ClientReady, async () => {
     console.log(`Logged in as ${bot.user.username}`);
-    
-    try {
-        const channel = await bot.channels.fetch("1529130651993899190");
-        await channel.send("@everyone Hello, world! (test).");
-        console.log("Startup message sent successfully.");
-    } catch (error) {
-        console.error("Failed to find the channel or send the message:", error);
-    }
+    cron.schedule('0 20 * * *', async () => {
+        console.log("Running daily Steam sale check...");
+        
+        try {
+            const channel = await bot.channels.fetch("1529130651993899190");
+            await channel.send("@everyone Перевірка таймера! Бот прокинувся.");
+            console.log("Timer message sent successfully.");
+            
+        } catch (error) {
+            console.error("Failed to find the channel or send the message:", error);
+        }
+    }, {
+        timezone: "Europe/Kyiv"
+    });
 });
 
 bot.login(process.env.DISCORD_TOKEN);
